a71f70e8d7353c2beeee437d2a02eaf7a1d969dc
ungrianzki <93168957+ungrianzki@users.noreply.github.com>
2026-09-17 20:48:18 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bubble Tsek offline support. Change VERSION when you upload new files.
-const VERSION = 'bubbletsek-v6';
+const VERSION = 'bubbletsek-v7';
 const FILES = [
   './', './index.html', './manifest.webmanifest',
   './lib/xlsx.full.min.js', './lib/jspdf.umd.min.js', './lib/exceljs.min.js', './fonts/fonts.css',
